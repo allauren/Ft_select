4a47a0fcbac71acee12f9a4c0031266a377884d0
Alexis LAURENT <allauren@student.42.fr>
2018-04-09 11:14:48 +0200

    laisses les croire que j irai nul part, crapaud deviendra prince

--- srcs/del_lst.c
+++ srcs/del_lst.c
@@ -6,7 +6,7 @@
 /*   By: allauren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/07 00:56:09 by allauren          #+#    #+#             */
-/*   Updated: 2018/04/09 10:15:32 by allauren         ###   ########.fr       */
+/*   Updated: 2018/04/09 11:03:28 by allauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,18 @@ void		del_all_lst(t_list *lst)
 void		print_all_lst(t_list *lst)
 {
 	t_data *data;
+	static int i = 0;
 
 	if (lst)
 	{
+			set_caps("ue");
+			set_caps("se");
 		data = lst->content;
+		if (!i++)
+		{
+			set_caps("us");
+			set_caps("so");
+		}
 		ft_putendl_fd(data->name, get_elems(NULL)->fd);
 		if (!((t_data*)(lst->next->content))->start)
 			print_all_lst(lst->next);
